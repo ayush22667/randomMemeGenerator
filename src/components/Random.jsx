846c7020715d2508bb8916369ac4d01cd47a3952
ayush22667 <ayushanand667@gmail.com>
2024-10-20 13:08:27 +0530

    Meme generated

---
+++ src/components/Random.jsx
@@ -0,0 +1,21 @@
+import useGif from "../hooks/useGif";
+import Spinner from "./Spinner"; // Ensure this is used correctly
+
+const Random = () => {
+  const { gif, loading, fetchData } = useGif();
+
+  return (
+    <div className="w-1/2 h-[450px] bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+      <h1 className="text-3xl uppercase underline font-bold">A Random Gif</h1>
+      {loading ? <Spinner /> : <img src={gif} width="450" alt="Random GIF" />} {/* Added alt prop */}
+      <button
+        onClick={() => fetchData()}
+        className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold"
+      >
+        Generate
+      </button>
+    </div>
+  );
+};
+
+export default Random;
